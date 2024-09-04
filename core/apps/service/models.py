85ad612ec70c8269379ab53552f74585a6ffc50e
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-04 18:21:09 +0330

    add ServiceStep model

--- core/apps/service/models.py
+++ core/apps/service/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from utils.mixins.models import Timestampable
+from utils.mixins.models import Sortable, Timestampable
 
 
 class ServiceStepValue(Timestampable):
@@ -13,3 +13,16 @@ class ServiceStepValue(Timestampable):
     class Meta:
         verbose_name = _("Service Step Value")
         verbose_name_plural = _("Service Step Values")
+
+
+class ServiceStep(Timestampable, Sortable):
+    title = models.CharField(_("title"), max_length=255)
+    description = models.TextField(_("description"), null=True,blank=True)
+    values = models.ManyToManyField(ServiceStepValue, verbose_name=_("values"), related_name="steps", blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.pk}: {self.title}"
+
+    class Meta:
+        verbose_name = _("Service Step")
+        verbose_name_plural = _("Service Steps")
