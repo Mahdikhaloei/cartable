93b08c282da49e24236266f16f4331f513942540
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-04 18:20:36 +0330

    add ServiceStemValue model

--- core/apps/service/models.py
+++ core/apps/service/models.py
@@ -3,18 +3,13 @@ from django.utils.translation import gettext_lazy as _
 from utils.mixins.models import Timestampable
 
 
-class Service(Timestampable):
-    """
-    Service model for Cartable project.
-    """
+class ServiceStepValue(Timestampable):
     title = models.CharField(_("title"), max_length=255)
-    excerpt = models.CharField(_("excerpt"), max_length=255, null=True, blank=True)
-    description = models.TextField(_("description"), null=True, blank=True)
-    image = models.ImageField(_("image"), upload_to="services/", null=True, blank=True)
+    image = models.ImageField(_("image"), upload_to="service/step_value", null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.pk}: {self.title}"
+    def __str__(self) -> str:
+        return f"{self.pk}: {self.value}"
 
     class Meta:
-        verbose_name = _("Service")
-        verbose_name_plural = _("Services")
+        verbose_name = _("Service Step Value")
+        verbose_name_plural = _("Service Step Values")
