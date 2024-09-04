64109debe16db60bdb5cb67bfad644539c0ff254
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-04 18:50:07 +0330

    add ServiceStepAdmin

--- core/apps/service/admin.py
+++ core/apps/service/admin.py
@@ -1,4 +1,4 @@
-from apps.service.models import ServiceStepValue
+from apps.service.models import ServiceStep, ServiceStepValue
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
@@ -14,3 +14,22 @@ class ServiceStepValueAdmin(admin.ModelAdmin):
     search_fields = ("title",)
     ordering = ("pk",)
     readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(ServiceStep)
+class ServiceStepAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ("title", "description", "order")}),
+        (_("Relations"), {"fields": ("values", )}),
+        (_("Timestamp"), {"fields": ("created_at", "updated_at")}),
+    )
+    list_display = ("title", "created_at", "get_values")
+    list_filter = ("title", "values")
+    search_fields = ("title", "description")
+    filter_horizontal = ("values",)
+    ordering = ("order",)
+    readonly_fields = ("created_at", "updated_at")
+
+    @admin.display(description="Values")
+    def get_values(self, obj):
+        return obj.get_values
