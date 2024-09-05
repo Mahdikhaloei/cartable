bdffa6be77e5041da05c2f753b0dfb68cef6da81
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-05 19:18:40 +0330

    add serviceViewSet

--- core/apps/service/v1/views.py
+++ core/apps/service/v1/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from apps.service.models import Service
+from apps.service.v1.serializers import ServiceSerializer
+from core.utils.mixins.viewsets import MultipleFieldLookupMixin
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class ServiceViewSet(MultipleFieldLookupMixin, ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    http_method_names = ["get",]
+    permission_classes = [AllowAny,]
