fdcd628f15f0f015b6ce2f6e744a7b544b648152
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-05 19:19:30 +0330

    add Service route

--- core/config/api_router.py
+++ core/config/api_router.py
@@ -1,9 +1,11 @@
 from apps.user.v1.views import UserViewSet
+from core.apps.service.v1.views import ServiceViewSet
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 router.register("users", UserViewSet)
+router.register("services", ServiceViewSet)
 
 app_name = "api"
 
