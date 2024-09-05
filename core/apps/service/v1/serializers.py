1873c32a267d058d801af30008eaad1812c38e40
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-05 19:11:53 +0330

    add ServiceStepSerializer

--- core/apps/service/v1/serializers.py
+++ core/apps/service/v1/serializers.py
@@ -1,4 +1,4 @@
-from apps.service.models import ServiceStepValue
+from apps.service.models import ServiceStep, ServiceStepValue
 from rest_framework import serializers
 
 
@@ -6,3 +6,11 @@ class ServiceStepValueSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceStepValue
         fields = ["id", "title", "image"]
+
+
+class ServiceStepSerializer(serializers.ModelSerializer):
+    values = ServiceStepValueSerializer(many=True)
+
+    class Meta:
+        model = ServiceStep
+        fields = ["id", "title", "description", "order", "values"]
