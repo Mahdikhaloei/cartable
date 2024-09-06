24e69401aae9be19af33fd6c7f50bbf220f563a3
Mahdi Khaloei <mahdikhaloei5109@gmail.com>
2024-09-06 12:39:04 +0330

    add RequestViewSet and it's list

--- core/apps/service/v1/views.py
+++ core/apps/service/v1/views.py
@@ -1,5 +1,5 @@
-from apps.service.models import Service
-from apps.service.v1.serializers import ServiceSerializer
+from apps.service.models import Request, Service
+from apps.service.v1.serializers import RequestSerializer, ServiceSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from utils.mixins.viewsets import MultipleFieldLookupMixin
@@ -10,3 +10,18 @@ class ServiceViewSet(MultipleFieldLookupMixin, ModelViewSet):
     serializer_class = ServiceSerializer
     http_method_names = ["get",]
     permission_classes = [AllowAny,]
+
+
+class RequestViewSet(MultipleFieldLookupMixin, ModelViewSet):
+    queryset = Request.objects.all().order_by("-created_at")
+    serializer_class = RequestSerializer
+    permission_classes = [AllowAny,]
+
+    def list(self, request, *args, **kwargs):
+        current_user = request.user
+        if current_user.is_superuser:
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(user=current_user)
+        # If you want to give access for departments you can add your scenarioes here
+        return queryset
